1388d22a5f6897e40f9a45089c13b8736838cb31
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-02-03 16:01:24 +0000

    test

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'bulma'
 import './stylesheets/main.scss'
-import Start from './components/common/Start'
+import Start from './components/Common/Start'
 import Questions from './components/Questions'
-import Error from './components/common/Error'
+import Error from './components/Common/Error'
 const App = () => (
   <BrowserRouter>
     <Switch>
